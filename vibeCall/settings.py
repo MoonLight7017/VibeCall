2da6329e4946848bd46fd94d1923bfd7d118d9b7
MoonLight7017 <developermoon7017@gmail.com>
2025-07-20 08:37:17 +0530

    third commit

--- vibeCall/settings.py
+++ vibeCall/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
